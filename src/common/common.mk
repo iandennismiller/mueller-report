913337c62f1cc62c903348caac6d6ffd3b5bd33f
Ian Dennis Miller <ian@iandennismiller.com>
2019-06-03 15:59:10 -0400

    remove ligatures in HTML output

--- src/common/common.mk
+++ src/common/common.mk
@@ -23,19 +23,44 @@ build/main.html: main.tex
 	mkdir -p build ../../docs/images
 	htlatex main.tex "bookstyle,fn-in,charset=utf-8" " -cunihtf -utf8"
 
+	# set viewport meta tag
 	sed -e 's/\<!-- fn-in,charset=utf-8,html,xhtml --\>/\<meta name="viewport" content="width=device-width, initial-scale=1.0"\>/' main.html > main.html.new
 	mv main.html.new main.html
 
+	# replace horizontal rules with HTML hr tag
 	sed -e 's/\<p class="noindent" \>__________\<\/p\>/\<hr width="10%"\>/' main.html > main.html.new
 	mv main.html.new main.html
 
+	# words within redacted boxes are contiguous
 	sed -e 's/_ _/_■_/g' main.html > main.html.new
 	mv main.html.new main.html
 
+	# wrap redactions in span tag
 	sed -e 's/\([■_]\{2,\}\)/\<span class="redacted"\>\1\<\/span\>/g' main.html > main.html.new
 	mv main.html.new main.html
 
+	# separate words within redacted boxes
 	sed -e 's/_■_/_ _/g' main.html > main.html.new
+	mv main.html.new main.html
+
+	# remove ligatures for simpler render and search
+	sed -e 's/ﬃ/ffi/g' main.html > main.html.new
+	mv main.html.new main.html
+
+	# remove ligatures for simpler render and search
+	sed -e 's/ﬀ/ff/g' main.html > main.html.new
+	mv main.html.new main.html
+
+	# remove ligatures for simpler render and search
+	sed -e 's/ﬁ/fi/g' main.html > main.html.new
+	mv main.html.new main.html
+
+	# remove ligatures for simpler render and search
+	sed -e 's/ﬂ/fl/g' main.html > main.html.new
+	mv main.html.new main.html
+
+	# remove ligatures for simpler render and search
+	sed -e 's/ﬄ/ffl/g' main.html > main.html.new
 	mv main.html.new main.html
 
 	mv main.html build/main.html
