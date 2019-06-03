1f5cb81189f9b384ac025d0fba36c0c301138acc
Ian Dennis Miller <ian@iandennismiller.com>
2019-06-03 15:03:12 -0400

    contiguous redacted boxes when possible

--- docs/redacted.js
+++ docs/redacted.js
@@ -1,3 +1,4 @@
 e = document.querySelector("body");
 e.innerHTML = e.innerHTML.replace(/([_■]{2,})/g, "<span class='redacted'>$1</span>");
+e.innerHTML = e.innerHTML.replace(/\<\/span\>\s*\<span class="redacted"\>/mg, " ");
 // e.innerHTML = e.innerHTML.replace(/(\[■.+\])/g, "<span class='redacted'>$1</span>");
